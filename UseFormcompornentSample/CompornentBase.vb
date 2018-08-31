55fa01e781e7a0713b63565bc44f79d8de2989e2
Unknown <boranti1995@gmail.com>
2018-08-31 20:33:04 +0900

    不要部を削除

--- UseFormcompornentSample/CompornentBase.vb
+++ UseFormcompornentSample/CompornentBase.vb
@@ -1,5 +1,5 @@
 ﻿Public Class CompornentBase
-    Public Sub New(ByRef parent As Form, ByVal location As Point, Optional ByVal txt As String = "loaded")
+    Public Sub New()
         ' この呼び出しはデザイナーで必要です。
         InitializeComponent()
         ' InitializeComponent() 呼び出しの後で初期化を追加します。
@@ -8,11 +8,6 @@
         With Me
             .TopLevel = False
             .Visible = True
-            .Location = location
         End With
-
-        parent.Controls.Add(Me) '親コンポーネントに自分を加える
-
-        Button1.Text = txt
     End Sub
 End Class
--- UseFormcompornentSample/Parent.vb
+++ UseFormcompornentSample/Parent.vb
@@ -1,6 +1,5 @@
 ﻿Public Class Parent
     Private Sub Parent_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim compo1 = New CompornentBase(Me, New Point(0, 0))
-        Dim compo2 = New CompornentBase(Me, New Point(0, 200), "compo2")
+
     End Sub
 End Class
