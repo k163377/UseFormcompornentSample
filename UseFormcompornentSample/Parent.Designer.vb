5d6e80e9693ef72526c145ce7c77bcd6fdbad9f3
Unknown <boranti1995@gmail.com>
2018-08-31 20:46:20 +0900

    修正完了

--- UseFormcompornentSample/Parent.Designer.vb
+++ UseFormcompornentSample/Parent.Designer.vb
@@ -22,19 +22,29 @@ Partial Class Parent
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MyCompornent = New UseFormcompornentSample.CompornentBase()
         Me.SuspendLayout()
+        '
+        'MyCompornent
+        '
+        Me.MyCompornent.BackColor = System.Drawing.Color.SpringGreen
+        Me.MyCompornent.ClientSize = New System.Drawing.Size(200, 100)
+        Me.MyCompornent.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MyCompornent.Location = New System.Drawing.Point(81, 73)
+        Me.MyCompornent.Name = "MyCompornent"
+        Me.MyCompornent.Text = "Form1"
         '
         'Parent
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MyCompornent)
         Me.Name = "Parent"
         Me.Text = "Parent"
         Me.ResumeLayout(False)
 
-
-        Dim MyCompornent As New CompornentBase
-        Me.Controls.Add(MyCompornent)
     End Sub
+
+    Friend WithEvents MyCompornent As CompornentBase
 End Class
